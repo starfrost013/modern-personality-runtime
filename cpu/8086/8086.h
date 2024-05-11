8458b012369463a979782121e49ca8847ac2e515
starfrost013 <mario64crashed@gmail.com>
2024-05-11 22:16:19 +0100

    SBB and SBC

--- cpu/8086/8086.h
+++ cpu/8086/8086.h
@@ -106,6 +106,9 @@ const char* i8086_RmToString(uint8_t rm);
 void i8086_Add8(uint8_t* destination, uint8_t* source, bool adc);	// 8-bit ADD/ADC: Destination must be pointer to one of the 8-bit registers inside "basecpu" structure, or a pointer into the 8086's address space.
 void i8086_Add16(uint16_t* destination, uint16_t* source, bool adc);	// 16-bit ADD/ADC: Destination must be pointer to one of the 16-bit registers inside "basecpu" structure, or a pointer into the 8086's address space.
 
+void i8086_Sub8(uint8_t* destination, uint8_t* source, bool sbb);	// 8-bit ADD/ADC: Destination must be pointer to one of the 8-bit registers inside "basecpu" structure, or a pointer into the 8086's address space.
+void i8086_Sub16(uint16_t* destination, uint16_t* source, bool sbb);	// 16-bit ADD/ADC: Destination must be pointer to one of the 16-bit registers inside "basecpu" structure, or a pointer into the 8086's address space.
+
 // instruction decode
 i8086_modrm_t i8086_ModRM(bool w, uint8_t opcode, uint8_t modrm);	// Parse ModR/M byte
 
